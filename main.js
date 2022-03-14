5ac1edeffe0d61e4fff7ee950dd5ba87a716f77f
Madigatta <doucouremadi5@gmail.com>
2022-03-14 14:22:05 +0100

    1st commit

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-const helloWorld = React.createElement('h1', {}, 'Hello world!');
+// Exo1
+// const helloWorld = React.createElement('h1', {}, 'Hello world!')
+
+// ReactDOM.render(helloWorld, document.querySelector('#app'));
+/***************************************************************************** */
+//Exo2
+
+// const titre = "Hello World";
+// const element = <h1>{titre}</h1>
+
+// ReactDOM.render(element, document.querySelector("#app"));
+
+// Exo3
 
 
-// mauvaise pratique
 
 
-//ReactDOM.render(helloWorld, document.body);
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
